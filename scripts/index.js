9aec7305b4251fb38d60ba4f8c69975143b2b880
Sergey Belov <belstmb@yandex.ru>
2024-07-23 11:35:19 +0300

    Refactoring after review

--- scripts/index.js
+++ scripts/index.js
@@ -9,15 +9,16 @@
 // @todo: Вывести карточки на страницу
 
 
-// получение содержимого template через свойство content
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector('#card-template').content;  // получение содержимого template через свойство content
+const placesList = document.querySelector('.places__list');             // элемент галереи для последующего вывода в него готовых карточек
 
-// функция создания карточки с установкой значений вложенных элементов и обработчика клика
+                                                                        // функция создания карточки с установкой значений вложенных элементов и обработчика клика:
 function createCard(name, link, deleteCardFn) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__title').textContent = name;
-    cardElement.querySelector('.card__image').src = link; 
-    cardElement.querySelector('.card__image').alt = name;
+    const cardImage = cardElement.querySelector('.card__image');        // запись элемента в переменную во избежание повторного его поиска в DOM для указания значений атрибутов
+    cardImage.src = link; 
+    cardImage.alt = name;
     
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', deleteCardFn);
@@ -25,14 +26,13 @@ function createCard(name, link, deleteCardFn) {
     return cardElement;
 };
 
-//добавление на страницу всех карточек из исходного массива методом .forEach
+                                                                        //последовательное добавление в элемент галереи карточек, заполненных из исходного массива:
 initialCards.forEach((card) => {
     const cardElement = createCard(card.name, card.link, removeCard);
-    const placesList = document.querySelector('.places__list'); 
     placesList.append(cardElement);
 });
 
-//функция удаления для передачи в функцию создания карточки и вызова из обработчика клика
+                                                                        //функция удаления для передачи в функцию создания карточки и вызова из обработчика клика:
 function removeCard(evt) {
     const eventTarget = evt.target;
     const card = eventTarget.closest('.card');
