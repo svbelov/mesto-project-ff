fb793269e9e05e43aeeb1a9140c804d71b864ea3
Sergey Belov <belstmb@yandex.ru>
2024-08-17 22:14:17 +0300

    Project 6. Steps 4 and 5 are completed. Profile edit func and card add form

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,11 +14,19 @@ import { initialCards } from './cards.js';
 const cardTemplate = document.querySelector('#card-template').content;  // получение содержимого template через свойство content
 const placesList = document.querySelector('.places__list');             // элемент галереи для последующего вывода в него готовых карточек
 
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileEditPopup = document.querySelector('.popup_type_edit');
+
+const cardAddButton = document.querySelector('.profile__add-button');
+const cardAddPopup = document.querySelector('.popup_type_new-card');
+
+const popups = document.querySelectorAll('.popup');
+
                                                                         // функция создания карточки с установкой соотв. значений вложенных элементов и обработчика клика:
 function createCard(name, link, deleteCardFn) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__title').textContent = name;
-    const cardImage = cardElement.querySelector('.card__image');        // запись элемента в переменную во избежание повторного его поиска в DOM для указания значений атрибутов
+    const cardImage = cardElement.querySelector('.card__image');        // запись элемента в переменную во избежание повторного его поиска в DOM для указания значений атрибутов.
     cardImage.src = link; 
     cardImage.alt = name;
     
@@ -41,29 +49,38 @@ function removeCard(evt) {
     card.remove();
 };
 
-                                                                        // функция открытия попапа c добавлением обработчика, запускаемого по нажатию клавиши
+                                                                        //функция открытия попапа c добавлением обработчика, запускаемого по нажатию клавиши:
 function openPopup(popup) {
     popup.classList.add('popup_is-opened');
     document.addEventListener('keydown', closePopupByPressEsc);
 };
 
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profileEditPopup = document.querySelector('.popup_type_edit');
 
-const cardAddButton = document.querySelector('.profile__add-button');
-const cardAddPopup = document.querySelector('.popup_type_new-card');
+                                                                        //функция заполнения полей формы в попапе редактирования профиля текущими значениями
+function fillEditFormFields() {
+    const formEditProfile = document.forms['edit-profile'];
+    const inputName = formEditProfile.elements.name;
+    const inputDescription = formEditProfile.elements.description;
+
+    const name = document.querySelector('.profile__title').textContent;
+    const description = document.querySelector('.profile__description').textContent;
+
+    inputName.value = name;
+    inputDescription.value = description;
+}
 
                                                                         //обработчики кликов по кнопкам редактирования профиля/добавления карточки:
-
+                                                                       
 profileEditButton.addEventListener('click', () => {
     openPopup(profileEditPopup);
+    fillEditFormFields();
 });
 
 cardAddButton.addEventListener('click', () => {
     openPopup(cardAddPopup);
 })
 
-                                                                        //функция закрытия попапа со снятием слушателя события:
+                                                                        //функция закрытия любого попапа со снятием слушателя события:
 function closePopup(popup) {
     popup.classList.remove('popup_is-opened');
     document.removeEventListener('keydown', closePopupByPressEsc);
@@ -78,8 +95,6 @@ function closePopupByPressEsc(evt) {
 };
 
                                                                         //закрытие любого попапа кликом по кнопке или оверлею:
-const popups = document.querySelectorAll('.popup');
-
 popups.forEach((item) => {
     item.addEventListener('click', (evt) => {
         if(evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup')) {
@@ -88,8 +103,27 @@ popups.forEach((item) => {
     });
 });
 
+//обработка события submit при отправке формы
 
-
-
+const formElement = profileEditPopup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_description');
+                                                                        //функция обработчик "отправки" формы:
+function handleFormSubmit(evt) {
+    evt.preventDefault();                                               //отмена стандартной отправки формы.
+                                                                        //получение значений полей:
+    const nameValue = nameInput.value;
+    const jobValue = jobInput.value;
+                                                                        //выбор элементов для вставки значений полей:      
+    const nameProfile = document.querySelector('.profile__title');
+    const descriptionProfile = document.querySelector('.profile__description');
+                                                                        //вставка новых значений:    
+    nameProfile.textContent = nameValue;
+    descriptionProfile.textContent = jobValue;
+                                                                        //закрытие попапа после отправки формы: 
+    closePopup(profileEditPopup);
+}
+                                                                        //прикрепление обработчика к форме, слушатель события submit:
+formElement.addEventListener('submit', handleFormSubmit);
 
 
